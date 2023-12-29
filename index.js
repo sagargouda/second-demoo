0eaf943702e426cc3f5b208167d079b2f4d89ac9
sagar <sagarc2580@gmail.com>
2023-12-29 06:55:15 +0530

    some comments added

--- index.js
+++ index.js
@@ -1,66 +1,74 @@
 const form = document.getElementById("my-form");
 
 const candiesList = document.getElementById("candies");
+//?? render user list every time when browser starts
 document.addEventListener("DOMContentLoaded", function () {
   renderUserList();
 });
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-
+  //?? gett the vales from form inputs
   const ChooseCandy = document.getElementById("ChooseCandy").value;
   const description = document.getElementById("description").value;
   const price = document.getElementById("price").value;
   const quantity = document.getElementById("quantity").value;
 
+  //?? storing inputs in form of an  object
   const candy = { ChooseCandy, description, price, quantity };
 
-  let candies = JSON.parse(localStorage.getItem("candies")) || [];
+  // let candies = JSON.parse(localStorage.getItem("candies")) || [];
 
-  candies.push(candy);
+  // candies.push(candy);
 
+  //?? storing in crud crud typical backend stuf by sending it our candy object
   axios
     .post(
       "https://crudcrud.com/api/d86ea6272f924ccf99d5852893933460/appData",
       candy
     )
     .then((response) => {
+      //?? also showing the response in screen
       renderUserList(response.data);
       console.log(response);
     })
     .catch((err) => {
-      document.body.innerHTML =
-        document.body.innerHTML + "<h4>something went wrong</h4>";
+      //?? handling errors if endpoints expires
+      document.body.innerHTML += "error hai bhai";
       console.log(err);
     });
-
+  //?? clear form input values for new values
   document.getElementById("ChooseCandy").value = "";
   document.getElementById("description").value = "";
   document.getElementById("price").value = "";
   document.getElementById("quantity").value = "";
-
-  //renderUserList();
 });
-
+//?? this is where main thing happens
 function renderUserList() {
+  //?? fetching data from crud crud using get request
   axios
     .get("https://crudcrud.com/api/d86ea6272f924ccf99d5852893933460/appData")
     .then((response) => {
       const candies = response.data;
       candiesList.innerHTML = "";
+      //?? iterating over candies using a for each loop
       candies.forEach(function (candy) {
+        //?? creating a i element
         const li = document.createElement("li");
+        //?? appended a text node for it
         li.appendChild(
           document.createTextNode(
             `${candy.ChooseCandy} - ${candy.description} - ${candy.price} - ${candy.quantity}`
           )
         );
+        //?? appending it to candies list which is an ul element
         candiesList.appendChild(li);
-
+        //?? creating buy one element
         const buyOneButton = document.createElement("button");
         buyOneButton.innerHTML = "Buy One";
         buyOneButton.addEventListener("click", function () {
           if (candy.quantity > 0) {
+            //?? we reduce a candy every time we order something
             updateDetails(
               candy._id,
               candy.ChooseCandy,
@@ -68,6 +76,8 @@ function renderUserList() {
               candy.quantity - 1,
               candy.price
             );
+          } else {
+            document.body.innerHTML += "candies completed";
           }
         });
         li.appendChild(buyOneButton);
@@ -75,31 +85,42 @@ function renderUserList() {
         const buyTwoButton = document.createElement("button");
         buyTwoButton.innerHTML = "Buy Two";
         buyTwoButton.addEventListener("click", function () {
-          updateDetails(
-            candy._id,
-            candy.ChooseCandy,
-            candy.description,
-            candy.quantity - 2,
-            candy.price
-          );
+          if (candy.quantity > 0) {
+            updateDetails(
+              candy._id,
+              candy.ChooseCandy,
+              candy.description,
+              candy.quantity - 2,
+              candy.price
+            );
+          } else {
+            document.body.innerHTML += "candies completed";
+          }
         });
         li.appendChild(buyTwoButton);
 
         const buyThreeButton = document.createElement("button");
         buyThreeButton.innerHTML = "Buy Three";
         buyThreeButton.addEventListener("click", function () {
-          updateDetails(
-            candy._id,
-            candy.ChooseCandy,
-            candy.description,
-            candy.quantity - 3,
-            candy.price
-          );
+          if (candy.quantity > 0) {
+            updateDetails(
+              candy._id,
+              candy.ChooseCandy,
+              candy.description,
+              candy.quantity - 3,
+              candy.price
+            );
+          } else {
+            document.body.innerHTML += "candies completed";
+          }
         });
         li.appendChild(buyThreeButton);
+
+        //?? delete functionality
         const deleteButton = document.createElement("button");
         deleteButton.innerText = "Delete";
         deleteButton.addEventListener("click", function () {
+          //?? so candy._id so important
           deleteUser(candy._id, li);
         });
         li.appendChild(deleteButton);
@@ -109,6 +130,7 @@ function renderUserList() {
       console.log(error);
     });
 }
+//?? main delktete fucntionality
 function deleteUser(id, li) {
   axios
     .delete(
@@ -119,6 +141,7 @@ function deleteUser(id, li) {
     })
     .catch((error) => console.log(error));
 }
+//?? updating details
 function updateDetails(id, ChooseCandy, description, quantity, price) {
   axios
     .put(
